d06ba7e2303a4c4b0870367d29da8ee7440c3cd6
Matteo Simone Borghi <matteosimoneborghi@gmail.com>
2023-01-30 00:05:45 +0100

    added first interface

--- src/index.js
+++ src/index.js
@@ -1,29 +1,71 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client'
 import axios from 'axios'
-import DataTable from 'react-data-table-component';
+import DataTable, { defaultThemes } from 'react-data-table-component'
+
+//style
+import './css/index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Boostrap component
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 
 class Main extends React.Component {
+
     constructor() {
         super();
         this.state = {
             columns: [
                 {
                     name: 'nome',
-                    selector: row => row.nome
+                    selector: row => row.nome,
+                    sortable: true
                 },
                 {
                     name: 'bilancio',
-                    selector: row => row.bilancio
+                    selector: row => row.bilancio,
+                    sortable: true
                 }
             ],
             data: []
+        }
+
+        this.customStyles = {
+            header: {
+                style: {
+                    minHeight: '56px',
+                },
+            },
+            headRow: {
+                style: {
+                    borderTopStyle: 'solid',
+                    borderTopWidth: '1px',
+                    borderTopColor: defaultThemes.default.divider.default,
+                },
+            },
+            headCells: {
+                style: {
+                    '&:not(:last-of-type)': {
+                        borderRightStyle: 'solid',
+                        borderRightWidth: '1px',
+                        borderRightColor: defaultThemes.default.divider.default,
+                    },
+                },
+            },
+            cells: {
+                style: {
+                    '&:not(:last-of-type)': {
+                        borderRightStyle: 'solid',
+                        borderRightWidth: '1px',
+                        borderRightColor: defaultThemes.default.divider.default,
+                    },
+                },
+            },
         }
     }
 
@@ -32,7 +74,28 @@ class Main extends React.Component {
             <Container fluid>
                 <Row>
                     <Col>
-                        <DataTable columns={this.state.columns} data={this.state.data}/>
+                        <Card>
+                            <Card.Header>
+                                <Row>
+                                    <Col className="align-start">
+                                        <h6>Conti Corrente</h6>
+                                    </Col>
+                                    <Col className="align-end">
+                                        <Button variant="primary" size="sm">
+                                            Aggiungi Conto Corrente
+                                        </Button>
+                                    </Col>
+                                </Row>
+                            </Card.Header>
+                            <Card.Body className="no-padding">
+                                <DataTable
+                                    columns={this.state.columns}
+                                    defaultSortFieldId={1}
+                                    customStyles={this.customStyles}
+                                    dense
+                                    data={this.state.data}/>
+                            </Card.Body>
+                        </Card>
                     </Col>
                 </Row>
             </Container>
